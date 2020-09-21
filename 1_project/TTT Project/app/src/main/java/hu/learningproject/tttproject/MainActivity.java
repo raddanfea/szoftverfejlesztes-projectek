ba20cc63564d901824a2e0092393fb892ea08e5a
Simion Tamás <tamas.simon12@gmail.com>
2020-09-21 23:24:00 +0300

    Created, Name Screen + transiton from main to name screen

--- 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/MainActivity.java
+++ 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/MainActivity.java
@@ -2,13 +2,32 @@ package hu.learningproject.tttproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+  private Button play_button;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    play_button = (Button) findViewById(R.id.play_button);
+    play_button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        openNameScreen();
+      }
+    });
   }
+
+  public void openNameScreen() {
+    Intent target = new Intent(this, NameScreen.class);
+    startActivity(target);
+  }
+
 }
