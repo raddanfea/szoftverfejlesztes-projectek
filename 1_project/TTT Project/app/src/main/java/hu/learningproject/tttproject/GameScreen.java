a99f0cec6ddbb5dc2e55c0ba0d15da2208d87f4a
Simion Tamás <tamas.simon12@gmail.com>
2020-09-22 17:52:48 +0300

    Update, beirt nevek atadasa a game layoutra

--- 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/GameScreen.java
+++ 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/GameScreen.java
@@ -3,69 +3,81 @@ package hu.learningproject.tttproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameScreen extends AppCompatActivity {
 
     private MyDrawable mydrawing;
 
-    int x, y;
     int startX, startY, origoX, origoY;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
 
-        final ImageView image = findViewById(R.id.imV);
-        mydrawing = new MyDrawable(image);
+        TextView name1 = (TextView) findViewById(R.id.textView1);
+        TextView name2 = (TextView) findViewById(R.id.textView2);
 
-        startX = 0;
-        startY = 0;
-        origoX = 0;
-        origoY = 0;
+        String player1name = getIntent().getStringExtra("p1n");
+        String player2name = getIntent().getStringExtra("p2n");
+
+        name1.setText(player1name);
+        name2.setText(player2name);
+
+
+    final ImageView image = findViewById(R.id.imV);
+    mydrawing = new MyDrawable(image);
+
+    startX = 0;
+    startY = 0;
+    origoX = 0;
+    origoY = 0;
 
         image.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
 
-                int _x = (int)event.getX();
-                int _y = (int)event.getY();
+            int _x = (int)event.getX();
+            int _y = (int)event.getY();
 
-                Log.d("asd", "touched at: " + _x + ", " + _y);
+            Log.d("asd", "touched at: " + _x + ", " + _y);
 
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        Log.d("asd", "start");
-                        startX = _x;
-                        startY = _y;
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        origoX += (_x - startX)/2.63; // magic number
-                        origoY += (_y - startY)/2.63;
-                        startX = _x;
-                        startY = _y;
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    Log.d("asd", "start");
+                    startX = _x;
+                    startY = _y;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    origoX += (_x - startX)/2.63; // magic number
+                    origoY += (_y - startY)/2.63;
+                    startX = _x;
+                    startY = _y;
 
-                        mydrawing.fillBackground(Color.rgb(50, 50, 50));
-                        mydrawing.drawGid(origoX, origoY, 80);
-                        break;
-                    case MotionEvent.ACTION_UP:
+                    mydrawing.fillBackground(Color.rgb(50, 50, 50));
+                    mydrawing.drawGid(origoX, origoY, 80);
+                    break;
+                case MotionEvent.ACTION_UP:
 
-                        break;
-                }
-
-                image.invalidate();
-                return true;
+                    break;
             }
-        });
 
-    }
+            image.invalidate();
+            return true;
+        }
+    });
+
+}
 
 
 }
--- 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/NameScreen.java
+++ 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/NameScreen.java
@@ -1,11 +1,13 @@
 package hu.learningproject.tttproject;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class NameScreen extends AppCompatActivity {
 
@@ -26,7 +28,15 @@ public class NameScreen extends AppCompatActivity {
     }
 
     public void openGameScreen() {
-        Intent target2 = new Intent(this, GameScreen.class);
+
+        EditText player1name = (EditText) findViewById(R.id.p1name);
+        String player1_name = player1name.getText().toString();
+        EditText player2name = (EditText) findViewById(R.id.p2name);
+        String player2_name = player2name.getText().toString();
+
+        Intent target2 = new Intent( this, GameScreen.class);
+        target2.putExtra("p1n",player1_name);
+        target2.putExtra("p2n",player2_name);
         startActivity(target2);
     }
 }
