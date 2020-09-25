6e82a56e404db93472d89e2ec88bd0b7550cfbef
Simion Tamás <tamas.simon12@gmail.com>
2020-09-25 18:32:13 +0200

    Added High Scores Class

--- 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/MainActivity.java
+++ 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
   private Button play_button;
   private Button exit_button;
+  private Button high_scores_button;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +33,23 @@ public class MainActivity extends AppCompatActivity {
         System.exit(0);
       }
     });
+    high_scores_button = (Button) findViewById(R.id.highscores_button);
+    high_scores_button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        openHighScores();
+      }
+    });
   }
 
   public void openNameScreen() {
     Intent target1 = new Intent(this, NameScreen.class);
     startActivity(target1);
+  }
+
+  public void openHighScores() {
+    Intent target2 = new Intent(this, HighScores.class);
+    startActivity(target2);
   }
 
 }
