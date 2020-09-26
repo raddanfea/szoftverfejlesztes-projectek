c1626c6289daf23ab5c41f549a1f8ca3a2d8080f
raddanfea <radidani55@gmail.com>
2020-09-26 16:49:53 +0200

    Update; PlaySound Class and test soundfile

--- 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/MainActivity.java
+++ 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/MainActivity.java
@@ -1,13 +1,21 @@
 package hu.learningproject.tttproject;
 
-import androidx.appcompat.app.AppCompatActivity;
+import
+        androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.IOException;
+
+import hu.learningproject.tttproject.model.PlaySound;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
         openHighScores();
       }
     });
+
+    PlaySound playSound = new PlaySound();
+
+    playSound.Play(this,"test");
+
   }
 
   public void openNameScreen() {
---
+++ 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/model/PlaySound.java
@@ -0,0 +1,33 @@
+package hu.learningproject.tttproject.model;
+
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Handler;
+
+import hu.learningproject.tttproject.MainActivity;
+import hu.learningproject.tttproject.R;
+
+public class PlaySound {
+
+
+    public final void Play(Context cont, String name){
+
+        Uri myUri = Uri.parse("android.resource://hu.learningproject.tttproject/" + R.raw.test);
+
+        final MediaPlayer mp = MediaPlayer.create(cont, myUri);
+        mp.setLooping(true);
+        mp.start();
+
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          mp.seekTo(0);
+                                      }
+                                  }
+                ,mp.getDuration()-80);
+    }
+
+
+}
