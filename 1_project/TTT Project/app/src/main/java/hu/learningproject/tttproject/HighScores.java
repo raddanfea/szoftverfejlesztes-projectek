e62dfe19656ac0a20430d04c049eedbbf6a0a486
Simion Tamás <tamas.simon12@gmail.com>
2020-09-26 07:21:27 +0200

    Update; High Scores; Added back button

--- 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/HighScores.java
+++ 1_project/TTT Project/app/src/main/java/hu/learningproject/tttproject/HighScores.java
@@ -3,12 +3,27 @@ package hu.learningproject.tttproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HighScores extends AppCompatActivity {
+
+    private Button back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_high_scores);
+        setContentView(R.layout.activity_high_scores);back_button = (Button) findViewById(R.id.back_b);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backScreen();
+            }
+        });
+    }
+
+    public void backScreen() {
+        finish();
+        System.exit(0);
     }
 }
