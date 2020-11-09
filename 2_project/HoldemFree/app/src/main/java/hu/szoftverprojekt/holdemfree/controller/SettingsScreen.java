36e1803e75a275d22004d73c040e5fddfb599706
Puskás Máté <puskasmate2000@gmail.com>
2020-11-09 14:10:55 +0100

    Fejlesztés: SettingsScreen.java implementálása.

---
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/SettingsScreen.java
@@ -0,0 +1,18 @@
+package hu.szoftverprojekt.holdemfree.controller;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import hu.szoftverprojekt.holdemfree.R;
+
+import android.os.Bundle;
+
+
+public class SettingsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_screen);
+    }
+}
