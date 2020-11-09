e275f9628c6f434777f42a450ee442eb2fe6ff00
Puskás Máté <puskasmate2000@gmail.com>
2020-11-09 14:10:19 +0100

    Fejlesztés: NameScreen.java implementálása.

---
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/NameScreen.java
@@ -0,0 +1,16 @@
+package hu.szoftverprojekt.holdemfree.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hu.szoftverprojekt.holdemfree.R;
+
+import android.os.Bundle;
+
+
+public class NameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_screen);
+    }
+}
