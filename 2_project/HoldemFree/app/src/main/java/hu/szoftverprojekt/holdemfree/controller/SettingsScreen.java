7652866ea656053d2aaf58e3845d0d8e027f0639
Puskás Máté <puskasmate2000@gmail.com>
2020-11-14 15:36:29 +0100

    Fejlesztés: SettingScreen back gomb.

--- 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/SettingsScreen.java
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/SettingsScreen.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import hu.szoftverprojekt.holdemfree.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SettingsScreen extends AppCompatActivity {
@@ -14,5 +16,19 @@ public class SettingsScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
+
+        Button back_button = (Button)findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBackScreen();
+            }
+        });
     }
+
+    void openBackScreen() {
+        finish();
+        System.exit(0);
+    }
+
 }
