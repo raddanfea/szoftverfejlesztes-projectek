d3e3d9909687d03e8380603a671a9b15457ca150
krisztian2545 <krisztian2545@gmail.com>
2020-11-14 09:28:25 +0100

    Fejlesztés: ScreenUpdaterEventArgs frissítése

--- 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/model/ScreenUpdaterEventArgs.java
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/model/ScreenUpdaterEventArgs.java
@@ -14,13 +14,33 @@ public class ScreenUpdaterEventArgs {
   public int moneyOnBoard;
   public int currentBet;
   public int dealerOffset;
+  public int roundsEnded;
+  public int turnsEnded;
+  public int indexOfRaiser;
   // extend this if needed, but remember to update the createEventArgs() function in the GameLogic too
   
-  public ScreenUpdaterEventArgs(ArrayList<Player> players, ArrayList<Card> board, int moneyOnBoard, int currentBet, int dealerOffset) {
+  public ScreenUpdaterEventArgs(ArrayList<Player> players, ArrayList<Card> board, int moneyOnBoard, int currentBet, int dealerOffset, int round, int turn, int indexOfRaiser) {
     this.players = players;
     this.board = board;
     this.moneyOnBoard = moneyOnBoard;
     this.currentBet = currentBet;
     this.dealerOffset = dealerOffset;
+    this.roundsEnded = round;
+    this.turnsEnded = turn;
+    this.indexOfRaiser = indexOfRaiser;
+  }
+  
+  @Override
+  public String toString() {
+    return "ScreenUpdaterEventArgs{" +
+        "players=" + players +
+        ", board=" + board +
+        ", moneyOnBoard=" + moneyOnBoard +
+        ", currentBet=" + currentBet +
+        ", dealerOffset=" + dealerOffset +
+        ", round=" + roundsEnded +
+        ", turn=" + turnsEnded +
+        ", indexOfRaiser=" + indexOfRaiser +
+        '}';
   }
 }
