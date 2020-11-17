3bc9806bd16231728432b81f97c6439dd185b8c6
Simion Tamás <tamas.simon12@gmail.com>
2020-11-17 18:34:05 +0100

    Fejlesztés, Creating RulesScreen.java controller

---
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/RulesScreen.java
@@ -0,0 +1,15 @@
+package hu.szoftverprojekt.holdemfree.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import hu.szoftverprojekt.holdemfree.R;
+
+public class RulesScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules_screen);
+    }
+}
