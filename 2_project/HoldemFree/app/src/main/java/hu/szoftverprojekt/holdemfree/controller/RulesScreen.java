0c557d51a4c7157a174ff6498c42f546ca910603
Simion Tamás <tamas.simon12@gmail.com>
2020-11-17 21:59:39 +0100

    Fejlesztés, RulesScreen,java, webview a szabályokhoz

--- 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/RulesScreen.java
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/RulesScreen.java
@@ -3,6 +3,10 @@ package hu.szoftverprojekt.holdemfree.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.webkit.WebView;
+
 import hu.szoftverprojekt.holdemfree.R;
 
 public class RulesScreen extends AppCompatActivity {
@@ -11,5 +15,10 @@ public class RulesScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules_screen);
+
+        String htmlAsString = getString(R.string.rules);
+
+        WebView webView = (WebView) findViewById(R.id.rules);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
     }
 }
