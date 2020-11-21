c8e0452ededf4e42e217405e6b67a5072b7894c6
Simion Tamás <tamas.simon12@gmail.com>
2020-11-21 15:48:10 +0100

    Fejlesztés, hand_ranking.xml és HandRankingScreen.java létrehozása

---
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/HandRankingScreen.java
@@ -0,0 +1,16 @@
+package hu.szoftverprojekt.holdemfree.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hu.szoftverprojekt.holdemfree.R;
+
+public class HandRankingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hand_ranking);
+    }
+}
