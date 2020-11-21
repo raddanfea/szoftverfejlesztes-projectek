2836d9db1ef9c0b24645de19c12cb5c6d5ffdaee
Simion Tamás <tamas.simon12@gmail.com>
2020-11-21 16:57:19 +0100

    Fejlesztés, HandRankingScreen.java handranking.xml csatolása a webview widgethez

--- 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/HandRankingScreen.java
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/HandRankingScreen.java
@@ -3,6 +3,7 @@ package hu.szoftverprojekt.holdemfree.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import hu.szoftverprojekt.holdemfree.R;
 
@@ -12,5 +13,10 @@ public class HandRankingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hand_ranking);
+
+        String htmlAsString = getString(R.string.handranking);
+
+        WebView webView = (WebView) findViewById(R.id.handranking);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
     }
 }
