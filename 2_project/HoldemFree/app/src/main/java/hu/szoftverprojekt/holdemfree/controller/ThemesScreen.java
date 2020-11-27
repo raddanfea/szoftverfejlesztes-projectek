abd4410f40b1ac7980d08756ded9fdb0aba86645
Puskás Máté <puskasmate2000@gmail.com>
2020-11-27 18:26:22 +0100

    Fejlesztés: ThemesScreen.java hozzáadása.

---
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/ThemesScreen.java
@@ -0,0 +1,20 @@
+package hu.szoftverprojekt.holdemfree.controller;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import hu.szoftverprojekt.holdemfree.R;
+
+import android.os.Bundle;
+
+
+public class ThemesScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_themes_screen);
+
+    }
+
+}
