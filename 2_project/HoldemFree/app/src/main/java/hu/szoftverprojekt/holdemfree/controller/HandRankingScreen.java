354ab2605dffdf010ef63ca70c7e6c7099dee295
Simion Tamás <tamas.simon12@gmail.com>
2020-12-07 07:58:55 +0100

    Fejlesztés, back gomb funkcio hozzá adasa a rules és hand ranking screenhez

--- 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/HandRankingScreen.java
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/HandRankingScreen.java
@@ -3,8 +3,11 @@ package hu.szoftverprojekt.holdemfree.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 import hu.szoftverprojekt.holdemfree.R;
 
@@ -20,5 +23,18 @@ public class HandRankingScreen extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/" + "handranking.html");
         webView.setBackgroundColor(0);
+
+        Button back_button = (Button)findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBackScreen();
+            }
+        });
+    }
+
+    void openBackScreen() {
+        Intent target_menu = new Intent(this, Menu.class);
+        startActivity(target_menu);
     }
 }
--- 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/RulesScreen.java
+++ 2_project/HoldemFree/app/src/main/java/hu/szoftverprojekt/holdemfree/controller/RulesScreen.java
@@ -3,8 +3,11 @@ package hu.szoftverprojekt.holdemfree.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 import hu.szoftverprojekt.holdemfree.R;
 
@@ -20,5 +23,19 @@ public class RulesScreen extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setBackgroundColor(0);
         webView.loadUrl("file:///android_asset/" + "rules.html");
+
+        Button back_button = (Button)findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBackScreen();
+            }
+        });
+
+    }
+
+    void openBackScreen() {
+        Intent target_menu = new Intent(this, Menu.class);
+        startActivity(target_menu);
     }
 }
